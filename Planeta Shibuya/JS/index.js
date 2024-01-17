579fa186ca58ebc53fa99908265a0957aa943206
PC-FRAN\itsfg <itsfgarciarubio@gmail.com>
2024-01-17 02:15:50 +0100

    Landing Page Base.

--- Planeta Shibuya/JS/index.js
+++ Planeta Shibuya/JS/index.js
@@ -1,15 +1,19 @@
-// index.js
-// Lista de imágenes disponibles
 var imagenes = ["IMG/place-holder.jpg", "IMG/Aki.jpg", "IMG/Angel.png", "IMG/Nayuta.png"];
 var indiceImagen = 0;
 
-// Función para cambiar la imagen
-function cambiarImagen() {
-  // Incrementa el índice de la imagen (circular)
+function cambiarImagen(idImagen) {
   indiceImagen = (indiceImagen + 1) % imagenes.length;
-  // Actualiza la fuente de la imagen con la siguiente imagen
-  document.getElementById('imagenActual').src = imagenes[indiceImagen];
+  document.getElementById(idImagen).src = imagenes[indiceImagen];
 }
 
-// Asigna la función al evento click directamente
-document.getElementById('imagenActual').addEventListener('click', cambiarImagen);
+var imagenes2 = ["IMG/percyJackson.jpg", "IMG/annabeth.jpg", "IMG/nicodiangelo.jpg", "IMG/grover.png",];
+var indiceImagen2 = 0;
+
+function cambiarImagen2() {
+  indiceImagen2 = (indiceImagen2 + 1) % imagenes2.length;
+  document.getElementById('imagenActual2').src = imagenes2[indiceImagen2];
+}
+
+
+
+
